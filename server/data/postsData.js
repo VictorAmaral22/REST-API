4733ac74f7c0b746f34ddd372dbd911a306cf2de
victor-grandmasterwitcher <victortavamaral@gmail.com>
2021-06-15 09:38:49 -0300

    Roi

--- server/data/postsData.js
+++ server/data/postsData.js
@@ -4,3 +4,6 @@ exports.getPosts = function () {
     return database.query('select * from local.post');
 }
 
+exports.savePost = function (post) {
+    return database.one('insert into local.post (local, ')
+}

3d88a13a0a54cbed59f3f0d7fe261e3ed69f39ea
victor-grandmasterwitcher <victortavamaral@gmail.com>
2021-06-14 22:12:31 -0300

    Update basicao

--- server/test/teste.test.js
+++ server/test/teste.test.js
@@ -1,12 +1,24 @@
-const { default: axios } = require('axios');
-const axion = require('axios');
-const { TestWatcher } = require('jest');
+const axios = require('axios');
+const postsService = require('../service/postsService');
 
-TestWatcher('Should get posts', async function () {
+//Biblioteca responsável pela função generate abaixo
+const crypto = require('crypto');
+
+//Faz uma string aleatória para ajudar no test
+const generate = function (){
+    return crypto.randomBytes(20).toString('hex');
+}
+
+test('Should get posts', async function () {
+    //  given
+    const post1 = postsService.savePost({title: generate(), content: generate()});
+    const post2 = postsService.savePost({title: generate(), content: generate()});
+    const post3 = postsService.savePost({title: generate(), content: generate()});
+    //  when
     const response = await axios({
         url: 'http://localhost:3000/posts',
         method: 'get'
     });
     const posts = response.data();
-    expect(posts).toHaveLenght(5);
+    //  then
 })
